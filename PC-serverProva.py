a9d6c5308ab0b2cb736a400e63b40e42c089d488
Silvia Sottile <silviasottile94@gmail.com>
2018-08-02 15:03:57 +0200

    Fixed certificates on PC version 	To be added on RPi version

---
+++ PC-serverProva.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import psutil
+from opcua import ua, Server
+server = Server()
+server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+server.load_certificate("myCertificate.der")
+server.load_private_key("myPrivateKey.pem")
+server.set_security_policy([
+    ua.SecurityPolicyType.NoSecurity,
+    ua.SecurityPolicyType.Basic128Rsa15_SignAndEncrypt,
+    ua.SecurityPolicyType.Basic128Rsa15_Sign])
+
+uri = "silvia-notebook.opcua.it/freeopcua/server"
+idx = server.register_namespace(uri)
+
+objects = server.get_objects_node()
+
+myobj = objects.add_object(idx, "MyObject")
+core0temp = myobj.add_variable(idx, "Core0Temperature", psutil.sensors_temperatures()['coretemp'][1].current)
+core1temp = myobj.add_variable(idx, "Core1Temperature", psutil.sensors_temperatures()['coretemp'][2].current)
+core0temp.set_writable()
+core1temp.set_writable()
+
+# starting!
+server.start()
+
+try:
+    while True:
+        time.sleep(1)
+        core0temp.set_value(psutil.sensors_temperatures()['coretemp'][1].current)
+        core1temp.set_value(psutil.sensors_temperatures()['coretemp'][2].current)
+finally:
+    # close connection, remove subcsriptions, etc
+    server.stop()
+
